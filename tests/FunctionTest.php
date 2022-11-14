611b75cd970f300e6a420abd39c85b8bf5a9a62d
corey6 <>
2022-11-14 18:49:41 +0000

    initial commit

---
+++ tests/FunctionTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class StackTest extends TestCase
+{
+    public function testPushAndPop(): void
+    {
+        $stack = [];
+        $this->assertSame(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertSame('foo', $stack[0]);
+        $this->assertSame(1, count($stack));
+
+        $this->assertSame('foo', array_pop($stack));
+        $this->assertSame(0, count($stack));
+    }
+
+/*
+asdfljasdlfj
+asdfasldjasdflkaj
+askdjasdlfkj
+*/
+  
+    /**
+     * 
+     */
+    public function testDelete(): void {
+        $result = print_r("hi there",true);
+        $this->assertSame("hi there", $result);
+    }
+}
